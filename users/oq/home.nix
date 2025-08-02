76cf213e1ab0cbcd87a8779ceb0bedf5c6b5dc7a
0qln <linusnag@gmail.com>
2025-08-02 10:09:24 +0200

    spelling

--- users/oq/home.nix
+++ users/oq/home.nix
@@ -440,7 +440,7 @@ in
           }
         ];
 
-        # transparent bufferbar
+        # transparent bufferline
         extraConfigLuaPost = ''
           vim.g.transparent_groups = vim.list_extend(
             vim.g.transparent_groups or {},
