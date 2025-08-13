ed33d59fcc05872d1bd15efa60e8c32bd304d403
0qln <linusnag@gmail.com>
2025-08-13 05:35:56 +0200

    enable kitty cursor_trail

--- users/oq/kitty/default.nix
+++ users/oq/kitty/default.nix
@@ -8,6 +8,8 @@
     enable = true;
     settings = {
       background_opacity = 0.5;
+      cursor_trail = 1;
+      cursor_trail_decay = "0.1 0.4";
       font_family = "VictorMono Nerd Font";
     };
     themeFile = "Catppuccin-Mocha";
--- users/oq/nixvim/default.nix
+++ users/oq/nixvim/default.nix
@@ -531,19 +531,6 @@
         enable = true;
       };
       undotree.enable = true;
-      # did have glitches, so nononoo
-      # smear-cursor = {
-      #   enable = true;
-      #   settings = {
-      #     stiffness = 0.8; # :                     # 0.6      [0, 1]
-      #     trailing_stiffness = 0.5; # :            # 0.4      [0, 1]
-      #     stiffness_insert_mode = 0.7; # :         # 0.5      [0, 1]
-      #     trailing_stiffness_insert_mode = 0.7; # :# 0.5      [0, 1]
-      #     damping = 0.8; # :                       # 0.65     [0, 1]
-      #     damping_insert_mode = 0.8; # :           # 0.7      [0, 1]
-      #     distance_stop_animating = 0.5; # :       # 0.1      > 0
-      #   };
-      # };
       lualine.enable = true;
       transparent = {
         enable = true;
