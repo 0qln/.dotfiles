77c6b2fa290d29b4faacde38a8dbd359f623482d
0qln <linusnag@gmail.com>
2025-08-31 00:31:43 +0200

    lg side by side diffs

--- home/oq/modules/git/default.nix
+++ home/oq/modules/git/default.nix
@@ -3,6 +3,7 @@
   home.packages = with pkgs; [
     git
     gh
+    delta
   ];
 
   programs.git = {
@@ -13,8 +14,15 @@
 
   programs.lazygit = {
     enable = true;
+    # https://github.com/jesseduffield/lazygit/blob/master/docs/Config.md
     settings = {
-      #TODO: https://github.com/jesseduffield/lazygit/blob/master/docs/Config.md
+      # https://github.com/jesseduffield/lazygit/issues/155#issuecomment-2260986940
+      git = {
+        paging = {
+          colorArg = "always";
+          pager = "delta --paging=never -s";
+        };
+      };
     };
   };
 
