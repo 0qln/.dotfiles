6e5668617010fd8367eb11232905ad746fea3905
0qln <linusnag@gmail.com>
2025-09-08 23:21:59 +0200

    refactoring and fixes

--- hosts/lif/sops.nix
+++ hosts/lif/sops.nix
@@ -4,26 +4,31 @@
   lib,
   ...
 }: let
-  mkSopsDependency = name: script: {
-    ${name}.text = script;
-    setupSecrets.deps = [name];
-  };
-
-  mkAgeIdentity = name: rec {
-    # use a combined key file instead of `path = "${root}/${name}"`,
-    # this way, sops can automatically fallback to use the backup key.
-    file = ../../yubis/${name};
+  mkAgeIdentity = name: file: rec {
     root = "/root/.config/sops/age/";
-    path = "${root}/yubi-combined";
-    name = name;
+    path = "${root}/${name}";
+    inherit name;
+    inherit file;
   };
 
-  ageIdentities = {
-    yubi-1 = mkAgeIdentity "yubi-1/age-yubikey-identity-ca0b293d.txt";
-    yubi-2 = mkAgeIdentity "yubi-2/age-yubikey-identity-7432b76e.txt";
+  mkYubiIdentity = name:
+    mkAgeIdentity
+    name
+    ../../yubis/${name};
+
+  yubiIdentities = {
+    yubi-1 = mkYubiIdentity "yubi-1/age-yubikey-identity-ca0b293d.txt";
+    yubi-2 = mkYubiIdentity "yubi-2/age-yubikey-identity-7432b76e.txt";
   };
 
-  flatten = xs: builtins.foldl' (acc: s: acc // s) {} xs;
+  ageIdentities =
+    yubiIdentities
+    // {
+      # age-keys = mkAgeIdentity "oq.age" "/home/oq/.config/sops/age/keys.txt";
+    };
+
+  ageIdentitiesSecrets = "sops/age/__all-keys.g.txt";
+  ageIdentitiesFile = "/run/secrets/${ageIdentitiesSecrets}";
 in {
   imports = [
     inputs.sops-nix.nixosModules.sops
@@ -31,7 +36,9 @@ in {
 
   sops = {
     defaultSopsFormat = "yaml";
-    age.keyFile = ageIdentities.yubi-1.path;
+    # use a combined key file.
+    # this way, sops can automatically fallback to use the backup key.
+    age.keyFile = ageIdentitiesFile;
   };
 
   environment.systemPackages = with pkgs; [
@@ -39,48 +46,73 @@ in {
   ];
 
   # https://github.com/NixOS/nixpkgs/blob/0d00f23f023b7215b3f1035adb5247c8ec180dbc/nixos/modules/system/activation/activation-script.nix
-  system.activationScripts =
-    # Copy the age identity to somewhere outside of the nix store since, sops
+  system.activationScripts = let
+    # Copy the age identities to somewhere outside of the nix store, since sops
     # does not allow paths to the nix store.
-    (flatten (lib.attrsets.mapAttrsToList
-      (name: identity:
-        mkSopsDependency "createIdentityCopy_${name}" (with identity; ''
-          mkdir -p "$(dirname "${path}")"
-          cat "${file}" >> "${path}"
-        ''))
-      ageIdentities))
-    //
-    # https://github.com/Mic92/sops-nix/issues/377#issuecomment-2926579189
-    (mkSopsDependency "setupYubikeyForSopsNix" (with pkgs; ''
+    identitiesScript = let
+      identities = lib.strings.concatStrings (lib.attrsets.mapAttrsToList
+        (name: identity:
+          with identity; ''
+            # === ${name} ===
+            ${builtins.readFile file}
+          '')
+        ageIdentities);
+    in {
+      name = "setupIdentitiesFile";
+      exec = ''
+        mkdir -p "$(dirname "${ageIdentitiesFile}")"
+        cat > "${ageIdentitiesFile}" << EOF
+        ${identities}
+        EOF
+      '';
+    };
 
-      PATH=$PATH:${lib.makeBinPath [age-plugin-yubikey]}
-      ${runtimeShell} -c "mkdir -p /var/lib/pcsc && ln -sfn ${ccid}/pcsc/drivers /var/lib/pcsc/drivers"
-
-      echo "pcscd: $(${toybox}/bin/pgrep pcscd)"
-
-      if ! ${toybox}/bin/pgrep pcscd > /dev/null ; then
-
-        # pcscd has some dependencies that sometimes aren't yet loaded when
-        # we start up pcscd, so we will wait a bit ...
-        # https://pcsclite.apdu.fr/
-
-        # TODO: find a better solution, maybe just copy the fucking systemd
-        # service module of nixpkgs in herer idkkkkkk
-
-        echo "waiting for dependencies..."
-        for i in {1..7}
-        do
-         echo "Loop spin:" $i
-          sleep 1
-        done
-        echo "done waiting"
-
-        echo starting pcscd...
-        ${pcsclite}/bin/pcscd
+    yubiScript = {
+      name = "setupYubikeyForSopsNix";
+      exec = with pkgs; ''
+        # idea: https://github.com/Mic92/sops-nix/issues/377#issuecomment-2926579189
+        PATH=$PATH:${lib.makeBinPath [age-plugin-yubikey]}
+        ${runtimeShell} -c "mkdir -p /var/lib/pcsc && ln -sfn ${ccid}/pcsc/drivers /var/lib/pcsc/drivers"
 
         echo "pcscd: $(${toybox}/bin/pgrep pcscd)"
-      fi
-    ''));
+
+        if ! ${toybox}/bin/pgrep pcscd > /dev/null ; then
+
+          # pcscd has some dependencies that sometimes aren't yet loaded when
+          # we start up pcscd, so we will wait a bit ...
+          # https://pcsclite.apdu.fr/
+
+          # TODO: find a better solution, maybe just copy the fucking systemd
+          # service module of nixpkgs in herer idkkkkkk
+
+          echo "waiting for dependencies..."
+          for i in {1..20}
+          do
+           echo "Loop spin:" $i
+            sleep 1
+          done
+          echo "done waiting"
+
+          echo starting pcscd...
+          ${pcsclite}/bin/pcscd
+
+          echo "pcscd: $(${toybox}/bin/pgrep pcscd)"
+        fi
+      '';
+    };
+  in {
+    ${identitiesScript.name} = {
+      deps = [];
+      text = identitiesScript.exec;
+    };
+    ${yubiScript.name} = {
+      deps = [identitiesScript.name];
+      text = yubiScript.exec;
+    };
+    ${"setupSecrets"} = {
+      deps = [yubiScript.name];
+    };
+  };
 
   # setting this to false fixes the problem where age-plugin-yubikey does not work, due to there being either
   # 2 or no pcscd processes running. One could probably modify the systemd service with some
