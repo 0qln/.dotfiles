3633e737b08b03860ff67e935878b0af6aa2150c
0qln <linusnag@gmail.com>
2025-09-30 21:05:45 +0200

    add NUR

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -2,24 +2,27 @@
   specialArgs,
   inputs,
   pkgs,
-  config,
   ...
 }: let
   utils = import ./utils;
   backupExtension = "hm-bac";
 in {
-  imports = [
-    inputs.home-manager.nixosModules.home-manager
+  imports = with inputs; [
+    home-manager.nixosModules.home-manager
+    nur.modules.nixos.default
   ];
 
-  home-manager.useGlobalPkgs = false;
-  home-manager.useUserPackages = true;
-  home-manager.extraSpecialArgs =
-    specialArgs
-    // {
-      inherit utils;
-      inherit backupExtension;
-    }
-    // extraArgs;
-  home-manager.backupFileExtension = backupExtension;
+  home-manager = {
+    useGlobalPkgs = false;
+    useUserPackages = true;
+    extraSpecialArgs =
+      specialArgs
+      // {
+        inherit (pkgs) nur;
+        inherit utils;
+        inherit backupExtension;
+      }
+      // extraArgs;
+    backupFileExtension = backupExtension;
+  };
 }
