62e25dcb6aef198f2326dabbc8b2d20f62026967
0qln <linusnag@gmail.com>
2025-10-09 20:07:02 +0200

    vscode config

--- home/oq/modules/vscode/appearance/profiles/kanagawa/default.nix
+++ home/oq/modules/vscode/appearance/profiles/kanagawa/default.nix
@@ -4,5 +4,6 @@ profile: {...}: {
     (import ../../modules/editor.nix profile)
     (import ../../icons/vsicons.nix profile)
     (import ../../colors/beeverfeever.kanagawa-vscode.nix profile)
+    (import ./editor.nix profile)
   ];
 }
