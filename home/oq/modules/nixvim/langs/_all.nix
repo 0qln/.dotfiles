9fd22be52150a03d26e4d540011027d55b449b23
0qln <linusnag@gmail.com>
2025-10-09 02:19:52 +0200

    oops

--- home/oq/modules/nixvim/langs/_all.nix
+++ home/oq/modules/nixvim/langs/_all.nix
@@ -1,7 +1,7 @@
 _: {
   imports = let
     paths = builtins.readDir ./.;
-    langs = builtins.filter (isLng && isMod) (builtins.attrNames paths);
+    langs = builtins.filter (n: isLng n && isMod n) (builtins.attrNames paths);
     store = map (l: ./${l}) langs;
     isMod = name: builtins.match ".*\\.nix$" name != null;
     isLng = name: name != "_all.nix";
