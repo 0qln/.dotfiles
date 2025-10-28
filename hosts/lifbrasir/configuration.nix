378efb8bb97e5b1ad9494bbc1540a48dbf8e3a71
0qln <linusnag@gmail.com>
2025-10-28 14:56:21 +0100

    cleanup lifbrasir/conf

--- hosts/lifbrasir/configuration.nix
+++ hosts/lifbrasir/configuration.nix
@@ -45,14 +45,6 @@ in {
       privateKeyFile = ./wireguard/0qln/private.key.secrets;
       externalInterface = "wlo1";
     })
-
-    #TODO: these are intested for lifbrasir. test first then activate them.
-    # (import ../../services/flake-upgrader {
-    #   flakeDir = "/root/.dotfiles";
-    # })
-    #
-    # (import ../../services/nixos-garbage-disposal {
-    # })
   ];
 
   sops = {
