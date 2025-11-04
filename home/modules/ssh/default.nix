7d50b1f9ffa2103f1fe199bfd36d8729fa5298d1
0qln <0qln@proton.me>
2025-11-05 00:57:45 +0100

    refactor home/ssh module

--- home/modules/ssh/default.nix
+++ home/modules/ssh/default.nix
@@ -18,7 +18,22 @@ in {
   config = mkIf cfg.enable {
     programs.ssh = {
       enable = true;
+      enableDefaultConfig = false;
       matchBlocks = mkMerge [
+        {
+          "*" = {
+            forwardAgent = false;
+            addKeysToAgent = "no";
+            compression = false;
+            serverAliveInterval = 0;
+            serverAliveCountMax = 3;
+            hashKnownHosts = false;
+            userKnownHostsFile = "~/.ssh/known_hosts";
+            controlMaster = "no";
+            controlPath = "~/.ssh/master-%r@%n:%p";
+            controlPersist = "no";
+          };
+        }
         {
           "0qln.duckdns.org" = {
             user = "root";
