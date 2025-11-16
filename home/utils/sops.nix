3d40147cbd9b8964e26ec7aecd80124d06facdad
0qln <0qln@proton.me>
2025-11-16 14:31:48 +0100

    WIP

--- home/utils/sops.nix
+++ home/utils/sops.nix
@@ -5,7 +5,8 @@
   inputs,
   lib,
   ...
-}: let
+}:
+with lib; let
   inherit (config.utils) userRuntimeDir;
 in {
   imports = [
@@ -54,49 +55,49 @@ in {
   };
 
   # https://github.com/Mic92/sops-nix/issues/377#issuecomment-2980260975
-  # systemd.user.services.sops-nix = {
-  #   Service = {
-  #     ExecStartPre = [
-  #       # Prevents this error on startup:
-  #       # GDBus.Error:org.freedesktop.DBus.Error.ServiceUnknown: The name org.freedesktop.Notifications was not provided by any .service files
-  #       "${pkgs.writeShellScript "sops-nix-start-pre-wait-for-notifications" ''
-  #         PIN=$(${pkgs.systemd}/bin/systemd-ask-password --no-tty "Enter YubiKey PIN:")
-  #         echo "display: $DISPLAY"
-  #         echo "authority: $XAUTHORITY"
-  #         export AGE_YUBIKEY_PIN="$PIN"
+  systemd.user.services.sops-nix = {
+    Service = {
+      ExecStartPre = [
+        # Prevents this error on startup:
+        # GDBus.Error:org.freedesktop.DBus.Error.ServiceUnknown: The name org.freedesktop.Notifications was not provided by any .service files
+        "${pkgs.writeShellScript "sops-nix-start-pre-wait-for-notifications" ''
+          PIN=$(${pkgs.systemd}/bin/systemd-ask-password --no-tty "Enter YubiKey PIN:")
+          echo "display: $DISPLAY"
+          echo "authority: $XAUTHORITY"
+          export AGE_YUBIKEY_PIN="$PIN"
 
-  #         if [ -z "$(${lib.getExe pkgs.yubikey-manager} list)" ]; then
-  #           until ${pkgs.systemd}/bin/busctl --user list \
-  #             | ${lib.getExe pkgs.ripgrep} -q org.freedesktop.Notifications; do
-  #             ${pkgs.coreutils}/bin/sleep 1
-  #           done
-  #         fi
-  #       ''}"
-  #       # Make sure to wait for the YubiKey insertion before starting the service
-  #       "${pkgs.writeShellScript "sops-nix-start-pre" ''
-  #         if [ -z "$(${lib.getExe pkgs.yubikey-manager} list)" ]; then
-  #           ${lib.getExe pkgs.libnotify} --urgency=critical --wait 'SOPS-Nix' 'Insert YubiKey to mount secrets...'
-  #           if [ -z "$(${lib.getExe pkgs.yubikey-manager} list)" ]; then
-  #             exit 1
-  #           fi
-  #         fi
-  #       ''}"
-  #     ];
-  #     Environment = lib.mkForce "DBUS_SESSION_BUS_ADDRESS=unix:path=%t/bus";
+          if [ -z "$(${getExe pkgs.yubikey-manager} list)" ]; then
+            until ${pkgs.systemd}/bin/busctl --user list \
+              | ${getExe pkgs.ripgrep} -q org.freedesktop.Notifications; do
+              ${pkgs.coreutils}/bin/sleep 1
+            done
+          fi
+        ''}"
+        # Make sure to wait for the YubiKey insertion before starting the service
+        "${pkgs.writeShellScript "sops-nix-start-pre" ''
+          if [ -z "$(${getExe pkgs.yubikey-manager} list)" ]; then
+            ${getExe pkgs.libnotify} --urgency=critical --wait 'SOPS-Nix' 'Insert YubiKey to mount secrets...'
+            if [ -z "$(${getExe pkgs.yubikey-manager} list)" ]; then
+              exit 1
+            fi
+          fi
+        ''}"
+      ];
+      Environment = mkForce "DBUS_SESSION_BUS_ADDRESS=unix:path=%t/bus";
 
-  #     Restart = "on-failure";
-  #     RestartSec = "5s";
-  #   };
-  #   Unit = let
-  #     deps = [
-  #       "dbus-user-session.service"
-  #       "graphical-session.target"
-  #       "gpg-agent.socket"
-  #       "pcscd.socket"
-  #     ];
-  #   in {
-  #     Wants = deps;
-  #     After = deps;
-  #   };
-  # };
+      Restart = "on-failure";
+      RestartSec = "5s";
+    };
+    Unit = let
+      deps = [
+        "dbus-user-session.service"
+        "graphical-session.target"
+        "gpg-agent.socket"
+        "pcscd.socket"
+      ];
+    in {
+      Wants = deps;
+      After = deps;
+    };
+  };
 }
