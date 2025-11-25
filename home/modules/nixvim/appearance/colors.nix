9fbf78773a1fc86490a4ca9bc8c8420576bf703a
0qln <0qln@proton.me>
2025-11-25 23:31:46 +0100

    make nixvim todo higlights optional

--- home/modules/nixvim/appearance/colors.nix
+++ home/modules/nixvim/appearance/colors.nix
@@ -13,91 +13,101 @@ in {
       default = null;
       description = "the colorscheme";
     };
+    highlight = {
+      todo = mkOption {
+        type = types.nullOr types.str;
+        default = null;
+        example = "Tomato";
+        description = "whether to higlight 'todo'";
+      };
+    };
   };
 
   config = {
-    programs.nixvim = {
-      extraPlugins = with pkgs.vimPlugins; [
-        everforest
-        rose-pine
-        melange-nvim
-        kanagawa-nvim
-        kanagawa-paper-nvim
+    programs.nixvim = mkMerge [
+      (mkIf (cfg.highlight.todo != null) {
+        match.TODO = "TODO";
+        highlight.Todo = {
+          fg = cfg.highlight.todo;
+        };
+      })
+      {
+        extraPlugins = with pkgs.vimPlugins; [
+          everforest
+          rose-pine
+          melange-nvim
+          kanagawa-nvim
+          kanagawa-paper-nvim
 
-        #TODO:
-        # https://github.com/ramojus/mellifluous.nvim
-        # https://github.com/dgox16/oldworld.nvim
-        # https://github.com/everviolet/nvim
-        # https://github.com/vague2k/vague.nvim
-      ];
-      colorschemes = mkIf (cfg.theme != null) {
-        ${cfg.theme}.enable = true;
-      };
+          #TODO:
+          # https://github.com/ramojus/mellifluous.nvim
+          # https://github.com/dgox16/oldworld.nvim
+          # https://github.com/everviolet/nvim
+          # https://github.com/vague2k/vague.nvim
+        ];
+        colorschemes = mkIf (cfg.theme != null) {
+          ${cfg.theme}.enable = true;
+        };
 
-      opts = {
-        # required by:
-        # - colorizer
-        termguicolors = true;
-      };
+        opts = {
+          # required by:
+          # - colorizer
+          termguicolors = true;
+        };
 
-      # https://github.com/catgoose/nvim-colorizer.lua
-      plugins.colorizer = {
-        enable = true;
-        settings = {
-          user_default_options = {
-            css = true;
+        # https://github.com/catgoose/nvim-colorizer.lua
+        plugins.colorizer = {
+          enable = true;
+          settings = {
+            user_default_options = {
+              css = true;
+            };
           };
         };
-      };
 
-      match.TODO = "TODO";
-      highlight.Todo = {
-        # todo: replace with wallust scheme color
-        fg = "Tomato";
-      };
+        plugins = {
+        };
 
-      plugins = {
-      };
+        autoCmd = [
+          {
+            # TODO: sometimes does not work idk
+            event = [
+              "ColorScheme"
+              "VimEnter"
+            ];
+            command = ":TransparentEnable";
+          }
+        ];
 
-      autoCmd = [
-        {
-          # TODO: sometimes does not work idk
-          event = [
-            "ColorScheme"
-            "VimEnter"
-          ];
-          command = ":TransparentEnable";
-        }
-      ];
+        # in case lualine is opaque again:
+        # https://www.reddit.com/r/neovim/comments/s4ud1d/comment/hsvesja/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
 
-      # in case lualine is opaque again:
-      # https://www.reddit.com/r/neovim/comments/s4ud1d/comment/hsvesja/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
+        # transparent bufferline
+        extraConfigLuaPost = ''
+          -- transparent bufferline
+          vim.g.transparent_groups = vim.list_extend(
+          vim.g.transparent_groups or {},
+            vim.tbl_map(function(v)
+              return v.hl_group
+            end, vim.tbl_values(require('bufferline.config').highlights))
+          )
 
-      # transparent bufferline
-      extraConfigLuaPost = ''
-        -- transparent bufferline
-        vim.g.transparent_groups = vim.list_extend(
-        vim.g.transparent_groups or {},
-          vim.tbl_map(function(v)
-            return v.hl_group
-          end, vim.tbl_values(require('bufferline.config').highlights))
-        )
-
-        -- tab indent colors
-        -- #TODO: make work with half-opaque colors
-        local highlight = {
-            "CursorColumn",
-            "Whitespace",
-        }
-        require("ibl").setup {
-            indent = { highlight = highlight, char = "" },
-            whitespace = {
-                highlight = highlight,
-                remove_blankline_trail = false,
-            },
-            scope = { enabled = false },
-        }
-      '';
-    };
+          -- tab indent colors
+          -- #TODO: make work with half-opaque colors
+          local highlight = {
+              "CursorColumn",
+              "Whitespace",
+          }
+          require("ibl").setup {
+              indent = { highlight = highlight, char = "" },
+              whitespace = {
+                  highlight = highlight,
+                  remove_blankline_trail = false,
+              },
+              scope = { enabled = false },
+          }
+        '';
+      }
+    ];
   };
 }
