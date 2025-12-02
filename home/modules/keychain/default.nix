9104ac0823f9ea9317ded6136703991f712d68ea
0qln <0qln@proton.me>
2025-12-02 05:39:40 +0100

    remove default keys from keychain init

--- home/modules/keychain/default.nix
+++ home/modules/keychain/default.nix
@@ -13,6 +13,7 @@ in {
   config = mkIf cfg.enable {
     programs.keychain = {
       enable = true;
+      keys = []; # set empty, since the default is id_rsa and we don't want that
     };
   };
 }
