9ac329d82350862430880bd7e05a6592d47b6fdc
0qln <0qln@proton.me>
2025-12-02 23:25:26 +0100

    configure screen

--- hosts/freyja/home-vars.nix
+++ hosts/freyja/home-vars.nix
@@ -3,8 +3,12 @@
     monitors = rec {
       devices = {
         center = {
-          name = "tmp";
-	  dim.s = 1.0;
+          name = "eDP-1";
+          dim = {
+            s = 1.0;
+            h = 1200;
+            w = 1920;
+          };
           workspaces = [1 2 3 4 5 6 7 8 9];
         };
       };
