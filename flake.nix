3043a042d8717d90fbfee61cc76fb8985f3ca315
0qln <0qln@proton.me>
2025-12-03 21:04:05 +0100

    use a slightly less goofy workaround for exporting functions into direnv

--- flake.nix
+++ flake.nix
@@ -239,81 +239,19 @@
             age-plugin-yubikey
           ];
 
-          mapFuncs = transform: (lib.mapAttrsToList transform funcs);
-
-          funcToDerivation = name: body:
-            writeShellApplication {
-              inherit name;
-              runtimeInputs = packages;
-              checkPhase = ["disable=SC2154"];
-              text = ''
-                ${body}
-              '';
-            };
-
           var-ensure = key: val:
           # bash
           ''[ -z "''$${key}" ] && export ${key}="${val}"'';
-
-          funcs = {
-            echo-test =
-              # bash
-              ''
-                echo 'test'
-              '';
-
-            update-sops-keys =
-              # bash
-              ''
-                fd -Ht f . | grep '.*secrets.*' | xargs sops updatekeys -y
-              '';
-
-            dots-prepare =
-              # bash
-              ''
-                cd ~/.dotfiles
-                nix flake update private
-                git add .
-              '';
-
-            dots =
-              # bash
-              ''
-                action="$1"
-                output="$2"
-
-                dots-prepare
-                nixos-rebuild "$action" --flake "$HOME/.dotfiles?submodules=1#$output" --impure --show-trace --sudo
-              '';
-
-            dots-remote =
-              # bash
-              ''
-                host="$1"
-                action="$2"
-                output="$3"
-
-                dots-prepare
-                nixos-rebuild --target-host "$host" "$action" --flake ~/.dotfiles?submodules=1#"$output" --impure --show-trace --sudo
-              '';
-
-            dots-lifbrasir =
-              # bash
-              ''
-                action="$1"
-
-                dots-remote "root@$lifbrasir" "$action" "lifbrasir"
-              '';
-          };
         in
           pkgs.mkShell {
             name = "dotfiles";
-            packages = packages ++ (mapFuncs funcToDerivation);
+            packages = packages;
 
             shellHook =
               # bash
               ''
                 ${var-ensure "lifbrasir" lifbrasir}
+                export PATH="$PWD/bin:$PATH"
               '';
           }
       );
