f7d1d178e70e15e33140b66dd64524f556164fda
0qln <0qln@proton.me>
2025-12-04 12:04:26 +0100

    freyja hyprshot fixes

--- home/modules/hypr/hyprshot.nix
+++ home/modules/hypr/hyprshot.nix
@@ -15,15 +15,23 @@ in
       home.packages = with pkgs; [
         hyprshot
       ];
-
-      wayland.windowManager.hyprland.settings.bind = let
+      wayland.windowManager.hyprland.settings = let
         hyprShotDir = config.vars.screenshots.dir;
         hyprshotExe = with pkgs; lib.getExe hyprshot;
         hyprshotCmd = ''HYPRSHOT_DIR="${hyprShotDir}" ${hyprshotExe} -z '';
-      in [
-        ", PRINT, exec, ${hyprshotCmd} -m output"
-        "SUPER, PRINT, exec, ${hyprshotCmd} -m window"
-        "SHIFT SUPER, PRINT, exec, ${hyprshotCmd} -m region"
-      ];
+      in {
+        bind = [
+          # for normal laptops that have a print button
+          ", PRINT, exec, ${hyprshotCmd} -m output"
+          "SUPER, PRINT, exec, ${hyprshotCmd} -m window"
+          "SHIFT SUPER, PRINT, exec, ${hyprshotCmd} -m region"
+        ];
+        binds = [
+          # for goofy ahh laptop keyboard that has an imposter on the keyboard >:3
+          "SHIFT_R & SUPER_L, S, exec, ${hyprshotCmd} -m output"
+          "SHIFT_R & SUPER_L & SHIFT_L, S, exec, ${hyprshotCmd} -m window"
+          "SHIFT_R & SUPER_L & SHIFT_L & ALT_L, S, exec, ${hyprshotCmd} -m region"
+        ];
+      };
     };
   }
