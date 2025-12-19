1406daaecea404f45efe26341c3851e8862b5eb8
0qln <0qln@proton.me>
2025-12-19 11:13:19 +0100

    youtube dislikes addon

--- home/modules/browsers/firefox/extensions/_common.nix
+++ home/modules/browsers/firefox/extensions/_common.nix
@@ -11,6 +11,7 @@ args @ {
     rycee.firefox-addons.ublock-origin
     rycee.firefox-addons.bitwarden
     rycee.firefox-addons.darkreader
+    rycee.firefox-addons.return-youtube-dislikes
   ]
   ++ [
     (import ./vimium-ff.nix args)
