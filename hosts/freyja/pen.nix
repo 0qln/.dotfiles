3599c278f9125d096ebc0351822a23a5d9f6610c
0qln <0qln@proton.me>
2026-01-29 18:23:38 +0100

    stylus pen drivers etc.

--- hosts/freyja/pen.nix
+++ hosts/freyja/pen.nix
@@ -1,3 +1,23 @@
-{...}: {
+{pkgs, ...}: {
   hardware.opentabletdriver.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    usbutils
+    xf86_input_wacom
+    kdePackages.wacomtablet
+  ];
+
+  services.xserver = {
+    modules = [pkgs.xf86_input_wacom];
+
+    extraConfig = ''
+      Section "InputClass"
+          Identifier "ELAN Pen to Wacom"
+          MatchUSBID "04f3:42cf" # Your exact device ID from xinput
+          MatchDevicePath "/dev/input/event*"
+          MatchIsTablet "true"
+          Driver "wacom"
+      EndSection
+    '';
+  };
 }
