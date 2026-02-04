276057ab5a38ddec7f4c52a7a791687dd6b96f5b
0qln <0qln@proton.me>
2026-02-04 08:47:38 +0100

    add theme structure for iliketrains

--- home/themes/iliketrains/home.nix
+++ home/themes/iliketrains/home.nix
@@ -1,19 +1,30 @@
-{pkgs, ...}: {
-  theme.wallpapers = rec {
-    arrangements = with images; {
-      "|-|" = {
-        left = horz1;
-        center = horz1;
-        right = horz1;
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+with lib; let
+  name = import ./name.nix;
+  cfg = config.themes.${name};
+in {
+  config = mkIf cfg.enable {
+    theme.wallpapers = rec {
+      arrangements = with images; {
+        "|-|" = {
+          left = horz1;
+          center = horz1;
+          right = horz1;
+        };
+        "-" = {
+          center = horz1;
+        };
       };
-      "-" = {
-        center = horz1;
-      };
-    };
-    images = {
-      horz1 = pkgs.fetchurl {
-        url = "https://w.wallhaven.cc/full/5g/wallhaven-5gx2q5.png";
-        hash = "sha256-2gpyEJ9GkTCnVMYbreKXB6QJTVvKc2Up8LHoPCHJ9Os=";
+      images = {
+        horz1 = pkgs.fetchurl {
+          url = "https://w.wallhaven.cc/full/5g/wallhaven-5gx2q5.png";
+          hash = "sha256-2gpyEJ9GkTCnVMYbreKXB6QJTVvKc2Up8LHoPCHJ9Os=";
+        };
       };
     };
   };
