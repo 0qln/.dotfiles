fc33fb5b1be443df893b7b2f66e83b79987fbea7
0qln <0qln@proton.me>
2026-04-16 14:01:02 +0200

    add cpw search engine

---
+++ home/modules/browsers/firefox/search-engines/chess-programming-wiki.nix
@@ -0,0 +1,6 @@
+_: {
+  name = "Chess Programming Wiki";
+  urls = [{template = "https://www.chessprogramming.org/index.php?search={searchTerms}";}];
+  iconMapObj."16" = "https://www.chessprogramming.org/images/favicon.ico";
+  definedAliases = ["@cpw"];
+}
