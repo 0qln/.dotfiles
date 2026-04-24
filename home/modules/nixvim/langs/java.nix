936399056d90befe486f29cac2fac8997da7bae0
0qln <0qln@proton.me>
2026-04-24 21:15:04 +0200

    configure java/lombok linting

--- home/modules/nixvim/langs/java.nix
+++ home/modules/nixvim/langs/java.nix
@@ -8,12 +8,33 @@
     plugins = {
       # this plugins also has nvim-dap integgration, which could be further configured.
       jdtls = {
-        enable = true;
+        # enable = true;
         settings = {
           cmd = [
             "jdtls"
-            {__raw = "'-data='..vim.fn.getcwd()..\"/.jdtls\"";}
+            "--jvm-arg=-javaagent:${pkgs.lombok}/share/java/lombok.jar"
+            "--jvm-arg=-Xbootclasspath/a:${pkgs.lombok}/share/java/lombok.jar"
+            "-data"
+            {__raw = "vim.fn.getcwd()..'/.jdtls'";}
           ];
+
+          settings.java = {
+            eclipse = {
+              downloadSources = true;
+            };
+            configuration = {
+              updateBuildConfiguration = "interactive";
+            };
+            maven = {
+              downloadSources = true;
+            };
+            implementationsCodeLens = {
+              enabled = true;
+            };
+            referencesCodeLens = {
+              enabled = true;
+            };
+          };
         };
       };
     };
