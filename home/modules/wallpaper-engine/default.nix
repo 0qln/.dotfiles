6889640dcdbb01bddc556edf6456f7ecad90a57f
0qln <0qln@proton.me>
2026-05-05 19:43:45 +0200

    workaround linus-wallpaperengine upstream issue

--- home/modules/wallpaper-engine/default.nix
+++ home/modules/wallpaper-engine/default.nix
@@ -1,5 +1,6 @@
 {
   config,
+  pkgs,
   lib,
   ...
 }: let
@@ -23,5 +24,34 @@ in
       services.linux-wallpaperengine = {
         enable = true;
       };
+
+      # Mai 05 17:48:48 lif linux-wallpaperengine[38807]: Duplicate argument --silent. Use /nix/store/zkwh7zy12cc4vcgmhzxlx9nhwm2m0s5n-linux-wallpaperengine-0-unstable-2026-03-01/bin/linux-wallpaperengine --help for>
+      systemd.user.services."linux-wallpaperengine".Service.ExecStart = let
+        cfg = config.services.linux-wallpaperengine;
+
+        args = wallpaper:
+          lib.concatStringsSep " " (
+            lib.cli.toGNUCommandLine {} {
+              screen-root = wallpaper.monitor;
+              inherit (wallpaper) scaling fps;
+              silent = wallpaper.audio.silent;
+              noautomute = !wallpaper.audio.automute;
+              no-audio-processing = !wallpaper.audio.processing;
+            }
+            ++ wallpaper.extraOptions
+          )
+          # This has to be the last argument in each group
+          + " --bg ${wallpaper.wallpaperId}";
+
+        cmd = wallpaper:
+          lib.getExe
+          cfg.package
+          + " "
+          + (lib.optionalString (cfg.assetsPath != null) " --assets-dir ${cfg.assetsPath} ")
+          + (lib.optionalString (cfg.clamping != null) "--clamping ${cfg.clamping} ")
+          + (args wallpaper)
+          + " &";
+      in
+        mkForce (getExe (pkgs.writeShellScriptBin "commands" (lib.strings.concatLines ((map cmd cfg.wallpapers) ++ ["wait"]))));
     };
   }
