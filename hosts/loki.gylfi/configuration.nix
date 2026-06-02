f9c933384f6b9e28b767c3fd999792c04507f2ef
0qln <0qln@proton.me>
2026-06-02 17:47:56 +0200

    use odoo in loki.gylfi

--- hosts/loki.gylfi/configuration.nix
+++ hosts/loki.gylfi/configuration.nix
@@ -1,10 +1,23 @@
-{...}: {
+{flake, ...}: {
+  imports = [
+    flake.nixosModules.odoo
+  ];
+
   home-manager = {
     users.oq = _: {
+      imports = [
+        flake.homeModules.odoo
+      ];
+
       settings = {
         enable = true;
         enableWorkSimple = true;
       };
+
+      modules = {
+        odoo.enable = true;
+      };
+
       private = {
         secrets.ssh = {
           server = true;
--- hosts/loki.gylfi/default.nix
+++ hosts/loki.gylfi/default.nix
@@ -2,6 +2,6 @@
   imports = [
     ../loki
     ../gylfi
-    # ./configuration.nix
+    ./configuration.nix
   ];
 }
