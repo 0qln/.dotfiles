c72e2bc163997559833930673efa11f4c5a0c40b
0qln <0qln@proton.me>
2026-06-08 17:22:44 +0200

    loki.lif config

--- hosts/loki.lif/configuration.nix
+++ hosts/loki.lif/configuration.nix
@@ -1,7 +1,38 @@
-{inputs, ...}: {
+{
+  inputs,
+  lib,
+  ...
+}:
+with lib; {
   imports = [
     inputs.private.nixosModules."lif"
 
     ./mount.nix
   ];
+
+  home-manager = {
+    users.oq = _: {
+      imports = [
+      ];
+
+      settings = {
+        enable = true;
+        enableWorkSimple = true;
+      };
+
+      modules = {
+        nixvim = {
+          transparency.enable = mkDefault true;
+        };
+      };
+
+      private = {
+        secrets.ssh = {
+          server = true;
+          work = true;
+          work-devops = true;
+        };
+      };
+    };
+  };
 }
