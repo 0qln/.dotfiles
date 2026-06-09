dce21989415f9da50e9e9db05a16bc0b31953684
0qln <0qln@proton.me>
2026-06-09 20:08:46 +0200

    add context7

--- home/modules/bash/default.nix
+++ home/modules/bash/default.nix
@@ -9,58 +9,85 @@ in
   with lib; {
     options.modules.bash = {
       enable = mkEnableOption "bash";
-    };
-    config = mkIf cfg.enable {
-      programs.direnv = {
-        enableBashIntegration = true;
-      };
-
-      programs.zoxide = {
-        enableBashIntegration = true;
-      };
-
-      programs.keychain = {
-        enableBashIntegration = true;
-      };
-
-      programs.bash = {
-        enable = true;
-        # Setting session variables normally is broken when using home-manager ;(
-        # context: https://github.com/nix-community/home-manager/issues/1011
-        initExtra = ''
-          export EDITOR="nvim"
-          alias cdf='cd $(fd --hidden --type d | fzf)'
-          ${
-            if config.programs.kitty.enable
-            then "alias ssh='kitten ssh --kitten share_connections=no'"
-            else ""
-          }
-          alias la='ll -a'
-          alias lg='lazygit'
-          # todo: put behind feature guard if ever using ueberzug again
-          # alias lf='lf-ueberzug'
-          alias nivm='nvim'
-          alias clearfetch='clear && ${vars.sysfetcher} && read _'
-
-          ${
-            with lib.strings;
-              concatLines (
-                map (
-                  x: let
-                    key = fixedWidthString x "." "";
-                    value = fixedWidthString ((x - 1) * 3) "../" "";
-                  in ''alias "${key}"="cd ${value}"''
-                ) (lib.lists.range 2 5)
-              )
-          }
-        '';
-        bashrcExtra = ''
-          shopt -s dotglob
-
-          set -o vi
-
-          eval "$(direnv hook bash)"
-        '';
+      secretFiles = mkOption {
+        type = types.attrs;
+        default = {};
+        example = {
+          name = ./secrets.env;
+        };
+        description = "Attrs of paths to env files that contain environment variables to be imported into the bash session.";
       };
     };
+    config = let
+      secretName = name: "vars/${name}";
+    in
+      mkIf cfg.enable {
+        programs.direnv = {
+          enableBashIntegration = true;
+        };
+
+        programs.zoxide = {
+          enableBashIntegration = true;
+        };
+
+        programs.keychain = {
+          enableBashIntegration = true;
+        };
+
+        sops.secrets = let
+          mkSecret = name: secrets:
+            nameValuePair (secretName name) {
+              sopsFile = secrets;
+              format = "dotenv";
+            };
+        in
+          attrsets.mapAttrs' mkSecret cfg.secretFiles;
+
+        programs.bash = {
+          enable = true;
+          # Setting session variables normally is broken when using home-manager ;(
+          # context: https://github.com/nix-community/home-manager/issues/1011
+          initExtra = ''
+            export EDITOR="nvim"
+            alias cdf='cd $(fd --hidden --type d | fzf)'
+            ${
+              if config.programs.kitty.enable
+              then "alias ssh='kitten ssh --kitten share_connections=no'"
+              else ""
+            }
+            alias la='ll -a'
+            alias lg='lazygit'
+            # todo: put behind feature guard if ever using ueberzug again
+            # alias lf='lf-ueberzug'
+            alias nivm='nvim'
+            alias clearfetch='clear && ${vars.sysfetcher} && read _'
+
+            ${
+              with lib.strings;
+                concatLines (
+                  map (
+                    x: let
+                      key = fixedWidthString x "." "";
+                      value = fixedWidthString ((x - 1) * 3) "../" "";
+                    in ''alias "${key}"="cd ${value}"''
+                  ) (lib.lists.range 2 5)
+                )
+            }
+
+            ${
+              with lib.strings;
+                concatLines (
+                  attrsets.mapAttrsToList (name: _: ''source "${config.sops.secrets.${secretName name}.path}"'') cfg.secretFiles
+                )
+            }
+          '';
+          bashrcExtra = ''
+            shopt -s dotglob
+
+            set -o vi
+
+            eval "$(direnv hook bash)"
+          '';
+        };
+      };
   }
